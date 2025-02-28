a3415516e19405f0ee7827a286c5243a8db7ffe6
2271389 <meoomimi.03@gmail.com>
2025-02-28 23:32:04 +0700

    update admin and readme

--- laravel-backend/routes/web.php
+++ laravel-backend/routes/web.php
@@ -28,15 +28,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    // config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.index');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     // config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('admin.index');
+//     })->name('dashboard');
+// });
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->name('dashboard');
 
 // Admin Logout Routes
 Route::get('/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
